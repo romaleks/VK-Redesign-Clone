4c65ff81268107fe72f732d4a33091c3bebf7d33
IDDna <romanov1371@gmail.com>
2022-12-22 18:49:11 +0300

    Save user data after sign up

--- src/pages/auth/userActions.js
+++ src/pages/auth/userActions.js
@@ -3,14 +3,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import { auth } from '../../firebase/firebase'
 
 export const signupUser = createAsyncThunk('user/signupUser', async data => {
+  const { email, password, firstName, lastName } = data
+
   try {
-    const response = await createUserWithEmailAndPassword(
-      auth,
-      data.email,
-      data.password
-    )
-    console.log(response)
-    return response.user
+    const response = await createUserWithEmailAndPassword(auth, email, password)
+
+    return [response.user, { firstName, lastName }]
   } catch (err) {
     return err.message
   }
--- src/redux/user.js
+++ src/redux/user.js
@@ -9,32 +9,44 @@ const initialState = {
     email: null,
     password: null,
     token: null,
+    firstName: null,
+    lastName: null,
   },
 }
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    setUserName(state, action) {
+      console.log(action.payload)
+      const { firstName, lastName } = action.payload
+      state.userData.firstName = firstName
+      state.userData.lastName = lastName
+    },
+  },
   extraReducers(builder) {
     builder
-      .addCase(signupUser.pending, (state, action) => {
+      .addCase(signupUser.pending, state => {
         state.loading = true
         state.error = null
       })
       .addCase(signupUser.fulfilled, (state, action) => {
         state.loading = false
-        state.userData = action.payload
+        const { email, accessToken } = action.payload[0]
+        const { firstName, lastName } = action.payload[1]
+        state.userData = { email, accessToken, firstName, lastName }
         console.log(state.userData)
       })
       .addCase(signupUser.rejected, (state, action) => {
         state.loading = false
-        console.log(1)
         state.error = action.error.message
       })
   },
 })
 
 export const selectUser = state => state.user
+
+export const { setUserName } = userSlice.actions
 
 export default userSlice.reducer
