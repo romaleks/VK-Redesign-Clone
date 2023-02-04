b682e7e616acc6d0fbce805b79ddcbf09b41b550
IDDna <romanov1371@gmail.com>
2023-02-04 18:48:13 +0300

    Implement loading page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,14 @@ import LeftSidebar from '../../components/LeftSidebar'
 import RightSidebar from '../../components/RightSidebar'
 import PostForm from './PostForm'
 import Post from './Post'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { selectUser, saveData } from '../../redux/user'
 import { useDispatch, useSelector } from 'react-redux'
 import { auth } from '../../firebase/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 
 const Home = () => {
+  const [render, setRender] = useState(false)
   const dispatch = useDispatch()
   const user = useSelector(selectUser)
 
@@ -18,12 +19,17 @@ const Home = () => {
       if (userAuth) {
         const { email, uid } = userAuth
         dispatch(saveData({ email, uid }))
-        console.log(user)
       }
     })
-  }, [dispatch])
+  }, [])
 
-  return (
+  useEffect(() => {
+    if (user.userData) {
+      setRender(true)
+    }
+  })
+
+  return render ? (
     <>
       <Header />
       <div className='m-auto flex max-w-7xl items-start gap-4 py-6 px-4'>
@@ -37,6 +43,8 @@ const Home = () => {
         <RightSidebar />
       </div>
     </>
+  ) : (
+    <p>Loading...</p>
   )
 }
 
