a0f6b6d0c5565192759435086d76f360068ee851
IDDna <romanov1371@gmail.com>
2023-02-15 22:12:10 +0300

    Open post form on focus

--- src/pages/home/PostForm.jsx
+++ src/pages/home/PostForm.jsx
@@ -35,27 +35,34 @@ const PostForm = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className='flex gap-2 rounded-lg border border-gray-300 px-4 py-2'
+      className='group flex flex-col gap-2 rounded-lg border border-gray-300 px-4 py-2'
     >
-      <input
-        type='text'
-        ref={titleRef}
-        placeholder={`${
-          user.userData ? user.userData.firstName : 'Loading...'
-        }, tell us something new...`}
-        className='flex-1 rounded-full bg-gray-200 px-6 py-1 text-lg font-medium outline-none
+      <div className='flex gap-2'>
+        <input
+          type='text'
+          ref={titleRef}
+          placeholder={`${
+            user.userData ? user.userData.firstName : 'Loading...'
+          }, tell us something new...`}
+          className='flex-1 rounded-full bg-gray-200 px-6 py-1 text-lg font-medium outline-none
         placeholder:text-gray-400 focus:bg-gray-300'
-      />
-      {['photos', 'videos', 'music'].map((btn, index) => {
-        return (
-          <div
-            key={index}
-            className='cursor-pointer rounded-lg bg-gray-200 p-1 duration-75 hover:bg-gray-300'
-          >
-            <img src={icons[btn]} alt={btn} className='h-8' />
-          </div>
-        )
-      })}
+        />
+        {['photos', 'videos', 'music'].map((btn, index) => {
+          return (
+            <div
+              key={index}
+              className='cursor-pointer rounded-lg bg-gray-200 p-1 duration-75 hover:bg-gray-300'
+            >
+              <img src={icons[btn]} alt={btn} className='h-8' />
+            </div>
+          )
+        })}
+      </div>
+      <textarea
+        placeholder='Description...'
+        className='hidden h-full w-full rounded-lg bg-gray-200 px-6 py-1 text-lg font-medium
+        outline-none placeholder:text-gray-400 focus:bg-gray-300 group-focus-within:block'
+      ></textarea>
     </form>
   )
 }
