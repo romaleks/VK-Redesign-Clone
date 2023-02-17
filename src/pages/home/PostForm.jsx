763f94bc0dbe1029aea7e2b08c17b27b99876e88
IDDna <romanov1371@gmail.com>
2023-02-17 22:21:47 +0300

    Don't close post form if some inputs aren't empty

--- src/pages/home/PostForm.jsx
+++ src/pages/home/PostForm.jsx
@@ -11,6 +11,7 @@ const PostForm = () => {
   const imageRef = useRef()
   const dispatch = useDispatch()
   const [imgPreview, setImgPreview] = useState()
+  const [inputState, setInputState] = useState(false)
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -37,7 +38,16 @@ const PostForm = () => {
 
   const handleImgChange = e => {
     const image = e.target.files[0]
+    setInputState(true)
     setImgPreview(image)
+  }
+
+  const handleInputChange = e => {
+    if (e.target.value) {
+      setInputState(true)
+    } else {
+      setInputState(false)
+    }
   }
 
   return (
@@ -54,6 +64,7 @@ const PostForm = () => {
           }, tell us something new...`}
           className='flex-1 rounded-full bg-gray-200 px-6 py-1 text-lg font-medium outline-none
         placeholder:text-gray-400 focus:bg-gray-300'
+          onChange={handleInputChange}
         />
         <div>
           <label
@@ -85,17 +96,23 @@ const PostForm = () => {
           <img src={icons.send} alt='send' className='h-8' />
         </div>
       </div>
-      <textarea
-        placeholder='Description...'
-        ref={descRef}
-        className='hidden h-full w-full rounded-lg bg-gray-200 px-6 py-1 text-lg font-medium
-        outline-none placeholder:text-gray-400 focus:bg-gray-300 group-focus-within:block'
-      ></textarea>
-      <img
-        src={imgPreview ? URL.createObjectURL(imgPreview) : null}
-        alt=''
-        className='w-full rounded-lg'
-      />
+      <div
+        className='hidden group-focus-within:block'
+        style={inputState ? { display: 'block' } : null}
+      >
+        <textarea
+          placeholder='Description...'
+          ref={descRef}
+          onChange={handleInputChange}
+          className='h-full w-full rounded-lg bg-gray-200 px-6 py-1 text-lg font-medium
+        outline-none placeholder:text-gray-400 focus:bg-gray-300'
+        ></textarea>
+        <img
+          src={imgPreview ? URL.createObjectURL(imgPreview) : null}
+          alt=''
+          className='w-full rounded-lg'
+        />
+      </div>
     </form>
   )
 }
