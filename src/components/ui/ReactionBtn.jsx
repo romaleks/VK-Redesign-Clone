84260e39337d6ade4dee35a2a26ef350be65554b
IDDna <romanov1371@gmail.com>
2023-02-19 22:53:45 +0300

    Implement instant like action

--- src/components/ui/ReactionBtn.jsx
+++ src/components/ui/ReactionBtn.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react'
 import icons from '../../data/icons'
 
 const ReactionBtn = ({ icon, count, onClick }) => {
+  const [curCount, setCurCount] = useState(count)
+
+  const handleClick = () => {
+    onClick()
+    setCurCount(prev => prev + 1)
+  }
+
   return (
     <div
-      onClick={onClick}
+      onClick={handleClick}
       className='flex cursor-pointer items-center gap-1.5 rounded-xl bg-gray-200 py-2 px-3 hover:bg-gray-300'
     >
       <img src={icons[icon]} className='h-6' />
       <span className='text-lg font-bold'>
-        {count !== undefined ? count : '—'}
+        {count !== undefined ? curCount : '—'}
       </span>
     </div>
   )
